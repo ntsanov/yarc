ef051ddf752dea7c14a736795859966d933ebc47
ntsanov <tsanov86@gmail.com>
2021-04-08 09:02:52 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"harmony_cli/cmd"
+	"io"
+	"log"
+)
+
+var (
+	config Config
+)
+
+func main() {
+	// rosetta-sdk-go uses logger to print the error as well as returning it
+	// We use our own json error wrapper so we don't really need it. Might be
+	// useful to turn it on for debugging though
+	log.SetOutput(io.Discard)
+	cmd.Execute()
+}
