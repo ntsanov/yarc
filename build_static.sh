c3a95581cc15c913231f910184a9c62dce905c7c
ntsanov <tsanov86@gmail.com>
2021-04-22 08:21:00 +0300

    Passing VERSION,COMMIT as env to docker image

--- build_static.sh
+++ build_static.sh
@@ -1,5 +1,10 @@
 #/bin/bash
+
+export VERSION=$(git rev-list --count HEAD)
+export COMMIT=$(git describe --always --long --dirty)
 docker run --rm -ti \
+    -e VERSION=${VERSION} \
+    -e COMMIT=${COMMIT} \
     -v ${PWD}/cli:/workspace \
     harmonyone/main make -C /workspace static
     
