0a74660bd4afe6c7750616443d536e63ce3846f2
ntsanov <tsanov86@gmail.com>
2021-04-22 08:35:52 +0300

    env variables

--- build_static.sh
+++ build_static.sh
@@ -1,10 +1,16 @@
 #/bin/bash
 
+if [ -n "$TRAVIS_BUILD_DIR" ]; then
+    CLI_DIR=${TRAVIS_BUILD_DIR}/cli
+else
+    CLI_DIR=${PWD}/cli 
+fi
+
 export VERSION=$(git rev-list --count HEAD)
 export COMMIT=$(git describe --always --long --dirty)
 docker run --rm -ti \
     -e VERSION=${VERSION} \
     -e COMMIT=${COMMIT} \
-    -v ${PWD}/cli:/workspace \
+    -v ${CLI_DIR}:/workspace \
     harmonyone/main make -C /workspace static
     
